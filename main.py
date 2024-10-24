09bce7fee1e15791b398a31432f5394b2d6c787d
tanyida <tanyida@hotmail.com>
2024-10-24 23:50:34 +0800

    tiktoken

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
+import tiktoken
 
 # Check if the password is correct.  
 if not check_password():  
@@ -23,6 +24,11 @@ if not check_password():
 # Sidebar contents
 with st.sidebar:
     st.title('WSHO Assessment Assistant!')
+
+
+def count_tokens(text):
+    encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    return len(encoding.encode(text))
 
 def main():
     load_dotenv()
