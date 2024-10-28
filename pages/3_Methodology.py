2e21e2914da26a9a6d2cc83c18848abbdb511524
tanyida <tanyida@hotmail.com>
2024-10-28 08:44:37 +0800

    image

--- pages/3_Methodology.py
+++ pages/3_Methodology.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 # Display the flowchart images for both calculators
-st.image('data\WSHO application.jpg', caption="Flowchart for user case 1") 
+st.image('./data/WSHO application.jpg', caption="Flowchart for user case 1") 
